6e80f5301fc899e8ef467c570ef2a40d1808d579
Bassim Beshry <70615080+Lunilux24@users.noreply.github.com>
2025-03-19 14:54:12 -0700

    Player/enemy health done. Enemy Damage done.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public float damage = 10f; // Adjustable for powerups. Dmg per projectile
     void OnTriggerEnter(Collider other)
     {
+        // Debugging to ensure collisons are being detected
         if (other.CompareTag("Enemy"))
         {
             Debug.Log("Hit an enemy!");
+        }
+
+        Health targetHealth = other.GetComponentInParent<Health>();
+
+        if (targetHealth != null)
+        {
+            targetHealth.TakeDamage(damage, gameObject);
+            Destroy(gameObject);
         }
     }
 }
